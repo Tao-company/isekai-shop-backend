f53278c8f71e2f307ab0aa4fcbd617b639e19b3d
rratchapol <rratchapol@gmail.com>
2025-05-31 14:24:36 +0700

    healthcheck api and cors middleware

--- main.go
+++ main.go
@@ -1,3 +1,16 @@
 package main
 
-func main() {}
+import (
+	"github.com/rratchapol/isekai-shop-api/config"
+	"github.com/rratchapol/isekai-shop-api/databases"
+	"github.com/rratchapol/isekai-shop-api/server"
+)
+
+func main() {
+	conf := config.ConfigGetting()
+	db := databases.NewPostgresDatabase(conf.Database)
+	server := server.NewEchoServer(conf, db.ConnectionGetting())
+
+	server.Start()
+
+}
